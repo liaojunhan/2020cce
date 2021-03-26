c59dda736b463b9ffcacc0702c1c4e18f6c5f1c3
liaojunhan <64124082+liaojunhan@users.noreply.github.com>
2021-03-26 11:36:25 +0800

    Add files via upload

---
+++ 3月25.cpp
@@ -0,0 +1,52 @@
+/*#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char line[10]="decline";
+    char line2[10]={'p','r','o','p','e','r','\0'};
+    printf("%s\n",line);
+    printf("%s\n",line2);
+    char line3[]="majority";
+    printf("%s\n",line3);
+    char line4[]={'m','a','j','o','r','i','t','y'};
+    printf("==%s==\n",line4);
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    char line[5][10]={"decline","proper","majority","bullet","shop"};
+    for(int i=0;i<5;i++)
+        printf("%s\n",line[i]);
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    printf("%d",'\0');
+}
+*/
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char line [10]="majority";
+    char line2 [10]="ask";
+    if(strcmp(line,line2)>0)
+        printf("左邊大\n");
+    else
+        printf("右邊大\n");
+}
+*/
+#include <stdio.h>
+#include <string.h>
+char a[100][10];
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+}
